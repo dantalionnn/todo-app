90e8118221b3eec44f15d05a60d3211e2d4b3268
dantalionnn <83746500+dantalionnn@users.noreply.github.com>
2021-07-02 07:47:27 +0300

    Add files via upload

---
+++ src/Components/todo-list/todo-list.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import TodoListItem from '../todo-list-item';
+import './todo-list.css';
+
+const TodoList = ({ items, onDelete, onToggleImportant, onToggleDone }) => {
+
+  const elements = items.map((item) => {
+
+    //destructing... returning label and important without id
+    const { id, ...itemProps } = item;
+
+    return (
+      <li key={id} className="list-group-item">
+        <TodoListItem {...itemProps}
+                      onDelete={() => onDelete(id)}
+                      onToggleImportant={() => onToggleImportant(id)}
+                      onToggleDone={() => onToggleDone(id)}/>
+      </li>
+    );
+  });
+
+  return (
+    <ul className="list-group todo-list">
+      { elements }
+    </ul>
+  );
+};
+
+export default TodoList;
